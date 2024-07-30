ddd2244f167e5fa26c91456ee078b18085cd9f7d
Hasan <hasan98209@gmail.com>
2024-07-30 17:41:37 +0530

    Models Setup

--- puppycompanyblog/__init__.py
+++ puppycompanyblog/__init__.py
@@ -1,7 +1,34 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
+#### SETUP DATABASE ####
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'data.sqlite')
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+
+#########################
+# LOGIN CONFIGS
+
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+
+login_manager.login_view = 'users.login'
+
+
+
+#########################
 
 from puppycompanyblog.core.views import core
 app.register_blueprint(core)
