6ef3a7b32ae4829989bc2677f77dca4a804c90df
Hasan <hasan98209@gmail.com>
2024-08-01 15:32:14 +0530

    complete blog project

--- puppycompanyblog/blog_posts/views.py
+++ puppycompanyblog/blog_posts/views.py
@@ -1,90 +1,71 @@
-
-from flask import render_template,url_for,flash,abort,request,redirect,flash,Blueprint
+from flask import render_template,url_for,flash, redirect,request,Blueprint
 from flask_login import current_user,login_required
+from puppycompanyblog import db
 from puppycompanyblog.models import BlogPost
 from puppycompanyblog.blog_posts.forms import BlogPostForm
-from puppycompanyblog import db
-
 
 blog_posts = Blueprint('blog_posts',__name__)
 
-# CREATE
 @blog_posts.route('/create',methods=['GET','POST'])
 @login_required
 def create_post():
-  form = BlogPostForm()
+    form = BlogPostForm()
 
-  if form.validate_on_submit():
+    if form.validate_on_submit():
 
-    blog_post = BlogPost(title=form.title.data,
-                         text=form.text.data,
-                         user_id = current_user.id,
+        blog_post = BlogPost(title=form.title.data,
+                             text=form.text.data,
+                             user_id=current_user.id
+                             )
+        db.session.add(blog_post)
+        db.session.commit()
+        flash("Blog Post Created")
+        return redirect(url_for('core.index'))
 
-                         )
-    
-    db.session.add(blog_post)
-    db.session.commit()
-    flash('Blog Post Created')
-    return redirect(url_for('core.index'))
-  
-  return render_template('create_post.html',form=form)
+    return render_template('create_post.html',form=form)
 
 
-
-# BLOG POST (VIEW)
+# int: makes sure that the blog_post_id gets passed as in integer
+# instead of a string so we can look it up later.
 @blog_posts.route('/<int:blog_post_id>')
 def blog_post(blog_post_id):
-  blog_post = BlogPost.query.get_or_404(blog_post_id)
-  return render_template('blog_post.html',
-                         title=blog_post.title,
-                         date=blog_post.date,post=blog_post)
+    # grab the requested blog post by id number or return 404
+    blog_post = BlogPost.query.get_or_404(blog_post_id)
+    return render_template('blog_post.html',title=blog_post.title,
+                            date=blog_post.date,post=blog_post
+    )
 
-
-
-
-# UPDATE
-@blog_posts.route('/<int:blog_post_id>/update',methods=['GET','POST'])
+@blog_posts.route("/<int:blog_post_id>/update", methods=['GET', 'POST'])
 @login_required
 def update(blog_post_id):
-  blog_post = BlogPost.query.get_or_404(blog_post_id)
+    blog_post = BlogPost.query.get_or_404(blog_post_id)
+    if blog_post.author != current_user:
+        # Forbidden, No Access
+        abort(403)
 
-  if blog_post.author != current_user:
-    abort(403)
-
-  form = BlogPostForm()  
-
-  if form.validate_on_submit():
-
-    blog_post.title = form.title.data
-    blog_post.text = form.text.data
-    db.session.commit()
-
-    flash('Blog Post Updated!')
-    return redirect(url_for('blog_posts.blog_post',blog_post_id=blog_post.id)) 
-  
-  elif request.method == 'GET':
-    form.title.data = blog_post.title
-    form.text.data = blog_post.text
-
-  return render_template('create_post.html',title='Updating',form=form)  
+    form = BlogPostForm()
+    if form.validate_on_submit():
+        blog_post.title = form.title.data
+        blog_post.text = form.text.data
+        db.session.commit()
+        flash('Post Updated')
+        return redirect(url_for('blog_posts.blog_post', blog_post_id=blog_post.id))
+    # Pass back the old blog post information so they can start again with
+    # the old text and title.
+    elif request.method == 'GET':
+        form.title.data = blog_post.title
+        form.text.data = blog_post.text
+    return render_template('create_post.html', title='Update',
+                           form=form)
 
 
-
-
-
-
-# DELETE
-@blog_posts.route('/<int:blog_post_id>/delete',methods=['GET','POST'])
+@blog_posts.route("/<int:blog_post_id>/delete", methods=['POST'])
 @login_required
 def delete_post(blog_post_id):
-
-  blog_post = BlogPost.query.get_or_404(blog_post_id)
-  if blog_post.author != current_user:
-    abort(403)
-
-  db.session.delete(blog_post)  
-  db.session.commit()
-  flash('Blog Post Deleted')
-  return redirect(url_for('core.index'))
-
-
+    blog_post = BlogPost.query.get_or_404(blog_post_id)
+    if blog_post.author != current_user:
+        abort(403)
+    db.session.delete(blog_post)
+    db.session.commit()
+    flash('Post has been deleted')
+    return redirect(url_for('core.index'))
